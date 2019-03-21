791399994baf08984250a0e05ec59436e6f36a66
Your Name <btrangcal@gmail.com>
2019-03-21 11:45:26 -0700

    initial react-router setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import {BrowserRouter,Route} from 'react-router-dom';
+
+const pageOne = ()=>{
+    return(
+        <div>
+            PAGE ONE
+        </div>
+    )
+}
+
+const pageTwo = ()=>{
+    return(
+        <div>
+            PAGE TWO
+            <button>CLICK ME?!</button>
+        </div>
+    )
+}
 
 const App =()=>{
-    return <div>App</div>
+    return <div>
+        <BrowserRouter>
+        <div>
+            <Route exact path="/" component={pageOne}/>
+            <Route  path="/pagetwo" component={pageTwo}/>
+        </div>
+        </BrowserRouter>
+    </div>
 }
 
 export default App;
