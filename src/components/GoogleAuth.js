228c4d30834fe300bbb9382d41abf0cff6a391df
Your Name <btrangcal@gmail.com>
2019-03-21 21:56:55 -0700

    added signIn status indicator

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -2,18 +2,37 @@ import React,{Component} from 'react';
 
 class GoogleAuth extends Component{
 
+    state={
+        isSignedIn:null
+    }
+
     componentDidMount(){
         window.gapi.load('client:auth2',()=>{
             window.gapi.client.init({
                 clientId:'372540513011-octip6laqocc36hlkjoi2fv4ccou7ok7.apps.googleusercontent.com',
                 scope:'email'
-            })
+            }).then(()=>{
+                this.auth = window.gapi.auth2.getAuthInstance();
+                this.setState({isSignedIn:this.auth.isSignedIn.get()});
+            }); 
         });   //use window since this is available on window scope
+    }
+
+    renderAuthButton(){
+        if(this.state.isSignedIn===null){
+            return <div>NO CLUE IF YOU ARE SIGNED IN</div>
+        }
+        else if (this.state.isSignedIn===true){
+            return<div>You're signed in </div>
+        }
+        else{
+            return <div>NOT SIGNED IN</div>
+        }
     }
     render(){
         return(
             <div>
-                GoogleAuth
+                {this.renderAuthButton()}
             </div>
         )
     }
