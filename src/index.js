b5694e74d91c75b0f8274300600811e8fb03ccaa
Your Name <btrangcal@gmail.com>
2019-03-22 20:05:54 -0700

    added redux dev tools

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
-
+import {createStore,applyMiddleware,compose} from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import App from './components/App';
 import reducers from './reducers'
 
-const store = createStore(reducers);
+
+const store = createStore(reducers,composeWithDevTools(applyMiddleware())
+);
 
 ReactDOM.render(
 <Provider store={store}>
